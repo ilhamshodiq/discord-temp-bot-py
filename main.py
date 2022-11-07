e861ca8af00503fadcb2113676472fed4cc91427
slaverikka <117674649+slaverikka@users.noreply.github.com>
2022-11-08 00:48:53 +0700

    Create main.py
    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+import discord
+import asyncio
+# from keep_alive import keep_alive
+from discord.ext import commands
+import random
+import datetime
+import pytz
+import youtube_dl
+import pafy
+import requests
+import json
+
+#something..
